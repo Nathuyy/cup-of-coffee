1e8318bf2cb0788aefd933bd69d7b4e9cdc781fb
nathy <nathalia.cericatto@gmail.com>
2024-09-04 16:21:43 -0300

    delete: spring secuity fix: bug 401 em clientes

--- src/main/java/cup_of_coffee/project/model/Cliente.java
+++ src/main/java/cup_of_coffee/project/model/Cliente.java
@@ -1,19 +1,70 @@
 package cup_of_coffee.project.model;
 
 import jakarta.persistence.*;
-
 import java.util.Set;
 
 @Entity
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Integer id;
+
     private String nomeCliente;
     private String email;
     private String senha;
 
     @OneToMany(mappedBy = "cliente")
     private Set<Pedido> pedidos;
+
+    public Cliente() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Set<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(Set<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nomeCliente='" + nomeCliente + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                '}';
+    }
 }
